0da7286ef0e1f108f20b5edddc188f5f46e61573
Davi de Carvalho <davi_carvalho96@hotmail.com>
2023-01-31 17:25:39 -0300

    Update env.mjs

--- apps/nextjs/src/env.mjs
+++ apps/nextjs/src/env.mjs
@@ -9,6 +9,10 @@ import { z } from "zod";
 export const server = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
+  NEXTAUTH_SECRET:
+    process.env.NODE_ENV === "production"
+      ? z.string().min(1)
+      : z.string().min(1).optional(),
 });
 
 /**
@@ -28,6 +32,7 @@ export const client = z.object({
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 

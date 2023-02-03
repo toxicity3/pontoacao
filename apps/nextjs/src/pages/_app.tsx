a4a5a899b759759ea9ad192046b1469e0ae88376
Davi de Carvalho <davi_carvalho96@hotmail.com>
2023-02-03 20:17:03 -0300

    refactor: retirando session do nextAuth

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -3,14 +3,10 @@ import type { AppType } from 'next/app';
 import { ptBR } from '@clerk/localizations';
 import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
-import type { Session } from 'next-auth';
 
 import { api } from '~/utils/api';
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { ...pageProps },
-}) => {
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <ClerkProvider
       localization={ptBR}

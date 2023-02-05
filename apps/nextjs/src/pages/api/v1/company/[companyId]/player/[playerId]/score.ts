cddde28ba633c3b7b7dceb30185a865575e2e304
Davi de Carvalho <davi_carvalho96@hotmail.com>
2023-02-05 12:30:09 -0300

    feat: criado endpoint para pegar score do player

---
+++ apps/nextjs/src/pages/api/v1/company/[companyId]/player/[playerId]/score.ts
@@ -0,0 +1,50 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import { asHandler, endpoint } from 'next-better-api';
+import { z } from 'zod';
+import { prisma } from '@acme/db';
+
+const getScore = endpoint(
+  {
+    method: 'get',
+    querySchema: z.object({
+      companyId: z.string(),
+      playerId: z.string(),
+    }),
+    responseSchema: z.union([
+      z.object({
+        message: z.string(),
+        errorCode: z.string(),
+      }),
+      z.object({
+        score: z.number(),
+      }),
+    ]),
+  },
+  async ({ query }) => {
+    const { companyId, playerId } = query;
+
+    const playerEvents = await prisma.playerHasEvent.findMany({
+      where: {
+        player: {
+          id: playerId,
+          companyId,
+        },
+      },
+    });
+
+    const score = playerEvents.reduce((acc, { score }) => {
+      if (!score) return acc;
+      acc += score;
+      return acc;
+    }, 0);
+
+    return {
+      status: 201,
+      body: {
+        score,
+      },
+    };
+  },
+);
+
+export default asHandler([getScore]);

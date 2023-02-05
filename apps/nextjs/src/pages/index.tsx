a69b83efdddfed16e09d2f3666a3460ac4d37583
Davi de Carvalho <davi_carvalho96@hotmail.com>
2023-02-05 16:16:21 -0300

    feat: removido section a mais e colocado id com link

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
   return (
     <>
       <GenericLayout className="bg-gray-900 text-white font-roboto">
-        <section className="w-screen h-screen">
+        <section id="inscreva-se" className="w-screen h-screen">
           <header className="w-full h-20 z-20 bg-gray-900 backdrop-blur-2xl relative lg:absolute">
             <div className=" relative z-20 max-w-7xl mx-auto flex h-full px-10 xl:px-5 justify-between items-center">
               <a href="#_">
@@ -128,8 +128,8 @@ const Home: NextPage = () => {
             </div>
 
             <div className="mt-12 text-center">
-              <a
-                href="#"
+              <Link
+                href="#inscreva-se"
                 className="mt-8 inline-flex items-center rounded border border-pink-600 bg-pink-600 px-8 py-3 text-white hover:bg-transparent focus:outline-none focus:ring active:text-pink-500"
               >
                 <span className="text-sm font-medium"> Inscreva-se </span>
@@ -148,57 +148,10 @@ const Home: NextPage = () => {
                     d="M17 8l4 4m0 0l-4 4m4-4H3"
                   />
                 </svg>
-              </a>
+              </Link>
             </div>
           </div>
         </section>
-
-        <section>
-          <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
-            <div className="max-w-3xl">
-              <h2 className="text-3xl font-bold sm:text-4xl">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod
-                alias doloribus impedit.
-              </h2>
-            </div>
-
-            <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-              <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full">
-                <Image
-                  src={
-                    'https://images.unsplash.com/photo-1496843916299-590492c751f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80'
-                  }
-                  alt="Party"
-                  fill={true}
-                  className="absolute inset-0 h-full w-full object-cover"
-                />
-              </div>
-
-              <div className="lg:py-16">
-                <article className="space-y-4">
-                  <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Aut qui hic atque tenetur quis eius quos ea neque sunt,
-                    accusantium soluta minus veniam tempora deserunt? Molestiae
-                    eius quidem quam repellat.
-                  </p>
-
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Dolorum explicabo quidem voluptatum voluptas illo
-                    accusantium ipsam quis, vel mollitia? Vel provident culpa
-                    dignissimos possimus, perferendis consectetur odit
-                    accusantium dolorem amet voluptates aliquid, ducimus tempore
-                    incidunt quas. Veritatis molestias tempora distinctio
-                    voluptates sint! Itaque quasi corrupti, sequi quo odit illum
-                    impedit!
-                  </p>
-                </article>
-              </div>
-            </div>
-          </div>
-        </section>
-
         <Footer />
       </GenericLayout>
     </>
